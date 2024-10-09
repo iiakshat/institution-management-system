c17a6dd57f2ffc70edd3a5a64f899fba48c1b85c
iiakshat <jakshat569@gmail.com>
2024-10-09 22:47:35 +0530

    Create app

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from . import db
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String(150), nullable=False)
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    age = db.Column(db.Integer, nullable=False)
+    major = db.Column(db.String(150), nullable=False)
